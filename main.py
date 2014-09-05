d106d79163c4cb8d6d1c081e0d73470089cd1206
akshairajendran <akshai.rajendran@gmail.com>
2014-09-05 11:26:40 -0500

    can't get it to work argh

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Root:
         %(msg)s<br />
         Departure Airport: <input type="text" name="airport_from"/><br />
         Arrival Airport: <input type="text" name="airport_to"/><br />
-        Date: <input type="date" name="date" value=""/><br />
+        Date: <input type="text" name="date"/><br />
         Carrier: <input type="text" name="carrier"/><br />
         Flight No.: <input type="number" name="flight_no"/><br />
         <input type="submit" value="Add Flight" />
@@ -48,7 +48,7 @@ class Root:
     @cherrypy.expose
     @require()
     def add_flight(self, airport_from=None, airport_to=None, date=None, carrier=None, flight_no=None):
-        if airport_from is None or airport_to is None or date == "" or carrier is None or flight_no is None:
+        if airport_from is None or airport_to is None or date is None or carrier is None or flight_no is None:
             return self.new_flight("Please enter all information")
         else:
             db_func.add_flight(cherrypy.request.login,airport_from,airport_to,date,carrier,flight_no)
