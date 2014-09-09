83647484efbc5763c3b4e9f3a29d620d9846db27
akshairajendran <akshai.rajendran@gmail.com>
2014-09-09 08:28:47 -0500

    added flight_update (just started)

---
+++ flight_update.py
@@ -0,0 +1,23 @@
+__author__ = 'arajendran'
+
+import sys
+from suds import null, WebFault
+from suds.client import Client
+import logging
+
+#open config file and get username and api key
+f = open('config.txt','r')
+credentials = [line[line.index(':')+1:len(line)].strip() for line in f]
+
+#credentials
+username = credentials[0]
+apiKey = credentials[1]
+url = 'http://flightxml.flightaware.com/soap/FlightXML2/wsdl'
+
+#setup logging and api
+logging.basicConfig(level=logging.INFO)
+api = Client(url, username=username, password=apiKey)
+
+#Get the weather
+result = api.service.Metar('KAUS')
+print result
