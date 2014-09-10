b58162e052feae3ecd91da22b9b31282cbbb6d40
akshairajendran <akshai.rajendran@gmail.com>
2014-09-10 08:08:49 -0500

    flight_serv receive msg works, need to send resp

--- flight_serve.py
+++ flight_serve.py
@@ -1,13 +1,33 @@
 __author__ = 'arajendran'
 
-from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
-from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCRequestHandler
-from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCDispatcher
+import SocketServer
+import json
 
-server = SimpleJSONRPCServer(('localhost', 13167), requestHandler=SimpleJSONRPCRequestHandler, logRequests=True)
+# from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+# from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCRequestHandler
+# from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCDispatcher
+#
+# server = SimpleJSONRPCServer(('localhost', 13167), requestHandler=SimpleJSONRPCRequestHandler, logRequests=True)
+#
+# server.register_function(pow)
+# server.register_function(lambda x,y: x+y, 'add')
+# server.register_function(lambda x: x, 'ping')
+# server.register_function(lambda x,y: x*y, 'mult')
+# server.serve_forever()
 
-server.register_function(pow)
-server.register_function(lambda x,y: x+y, 'add')
-server.register_function(lambda x: x, 'ping')
-server.register_function(lambda x,y: x*y, 'mult')
+class MyTCPServer(SocketServer.ThreadingTCPServer):
+    allow_reuse_address = True
+
+class MyTCPServerHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        try:
+            #take the data and print it
+            data = self.request.recv(1024).strip()
+            print data
+            # send some 'ok' back
+            self.request.sendall(json.dumps({"Content-type":"text/plain", 'result':'ok'}))
+        except Exception, e:
+            print "Exception while receiving message: ", e
+
+server = MyTCPServer(('127.0.0.1', 13167), MyTCPServerHandler)
 server.serve_forever()
