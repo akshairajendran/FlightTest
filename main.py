0790be8645525f614b333f590b91bcd8ffc2302a
akshairajendran <Akshai@akshai-rajendrans-macbook-pro.local>
2014-10-05 09:47:23 -0400

    got background image to work

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Root:
     def index(self):
         return """<html>
         <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" type="text/css" href="static/css/index.css"/>
         </head>
         <body>
